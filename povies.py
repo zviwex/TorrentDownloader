a007b2817158b139758811324206e4374fcfb659
ZviWex <zvikizviki@gmail.com>
2018-10-10 19:32:50 +0300

    Init

---
+++ povies.py
@@ -0,0 +1,90 @@
+"""
+Povies class
+"""
+
+import os
+import sys
+import subprocess
+import requests
+
+
+class Povies(object):
+    """
+    This class provides methods for movie requests
+    """
+    def __init__(self):
+        pass
+
+
+    def movies(self, limit):
+        """
+        - List some movies 
+        - Requires a limit
+        - Movies are:
+            * sorted by seeds.
+            * ordered n ascending order
+            * quality of 1080p
+        - Returns 50 movies at max with a default of 20
+        """
+        url = "https://yts.ag/api/v2/list_movies.json?sort=seeds&limit=%s&order_by=asc&quality=1080p" % limit
+        res = requests.get(url)
+        dic = res.json()
+        return dic['data']['movies']
+
+    
+    def search(self, query):
+        """
+        - Search for movies 
+        - Requires a search query
+        - Movies are:
+            * sorted by highest number of downloads.
+            * quality of 1080p
+        """
+        url = "https://yts.ag/api/v2/list_movies.json?query_term=%s&sort_by=download_count&quality=1080p" % query
+        res = requests.get(url)
+        dic = res.json()
+        return dic['data']['movies']
+
+
+    def movie(self, movie_id):
+        """
+        - Fetch a movie detail
+        - Requires a movie_id
+        """
+        url = "https://yts.ag/api/v2/movie_details.json?movie_id=%s" % movie_id
+        res = requests.get(url)
+        dic = res.json()
+        return dic['data']['movie']
+
+
+    def related(self, movie_id):
+        """
+        - Fetch movies related to a movie
+        - Requires a movie_id
+        """
+        url = "https://yts.ag/api/v2/movie_suggestions.json?movie_id=%s" % movie_id
+        res = requests.get(url)
+        dic = res.json()
+        return dic['data']['movies']
+
+    def download(self, movie_id):
+        """
+        - Download the torrent file for a particular movie
+        - Requires a movie_id
+        """
+        url = "https://yts.ag/api/v2/movie_details.json?movie_id=%s" % movie_id
+        res = requests.get(url)
+        dic = res.json()
+        torrent_url = dic['data']['movie']['torrents'][0]['url']
+        torrent_title = dic['data']['movie']['title_english']
+        r = requests.get(torrent_url)
+        with open(torrent_title+'.torrent', 'wb') as code:
+            code.write(r.content)
+        if sys.platform == 'linux2':
+            subprocess.call(['xdg-open', torrent_title+'.torrent'])
+        if sys.platform == 'win32':
+            os.startfile(torrent_title+'.torrent')
+        if sys.platform == 'darwin':
+            os.system('open ' + torrent_title+'.torrent')
+        else:
+            return {"Message": "Torrent file has been downloaded into current working directory"}
---
+++ torrentdownloader.py
@@ -0,0 +1,57 @@
+import os 
+import urllib3
+import sys
+from pprint import pprint
+import eztvit
+import povies
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def download_movie(name, quality=720):
+    p = povies.Povies() 
+    l_movies = p.search(name)
+    p.download(l_movies[0]['id'])
+
+def download_episode(series, season, episode):
+    all_episotes = eztvit.EztvIt().get_episodes(series)
+    episode = all_episotes[season][episode]
+    episode = sorted(episode, key=lambda k: k['size_mb'])[-1]
+    print("Downloading: {}, Size: {}".format(episode["release"], episode["size_mb"]))
+    magnet = episode['download']['magnet']
+    os.startfile(magnet)
+
+  
+def download_season(series, season):
+    all_episotes = eztvit.EztvIt().get_episodes(series)
+    season = all_episotes[season]
+    for key, value in season.iteritems():
+        episode = sorted(value, key=lambda k: k['size_mb'])[-1]
+        print("Downloading: {}, Size: {}".format(episode["release"], episode["size_mb"]))
+        magnet = episode['download']['magnet']
+        os.startfile(magnet)
+
+  
+def download_series(series):
+    all_episotes = eztvit.EztvIt().get_episodes(series)
+    for key, value in all_episotes.iteritems():
+        season = all_episotes[key]
+        for key, value in season.iteritems():
+            episode = sorted(value, key=lambda k: k['size_mb'])[-1]
+            print("Downloading: {}, Size: {}".format(episode["release"], episode["size_mb"]))
+            magnet = episode['download']['magnet']
+            os.startfile(magnet)
+
+
+
+def main():
+    if len(sys.argv) != 5:
+        print "Usage:"
+        print "{} m <movie name> - for movies"
+        print "{} e <tvshow name> <series number> <episode number> - for episodes"
+    if sys.argv[1][0] == "m":
+        download_movie(sys.argv[2])
+    else:
+        download_episode(sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+
+main()
